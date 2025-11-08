80e5e8d82c376e33de3b0034103f5eb83d8de8c7
Aaron Spiteri <azzmosphere@gmail.com>
2025-11-08 16:08:45 +1100

    modifying code to match interfaces

--- include/rr_state_mgm_srv/state_manager.hpp
+++ include/rr_state_mgm_srv/state_manager.hpp
@@ -7,12 +7,13 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rr_common_base/rr_constants.hpp"
 #include "rr_common_base/rr_state_mng_constants.hpp"
-#include "rr_interfaces/srv/state_batt_req.hpp"
-#include "rr_interfaces/srv/state_gps_req.hpp"
-#include "rr_interfaces/srv/state_image.hpp"
-#include "rr_interfaces/srv/state_imu.hpp"
-#include "rr_interfaces/srv/state_joy_req.hpp"
-#include "rr_interfaces/srv/state_range.hpp"
+#include "rr_interfaces/srv/battery_state.hpp"
+#include "rr_interfaces/srv/gps.hpp"
+#include "rr_interfaces/srv/image.hpp"
+#include "rr_interfaces/srv/imu.hpp"
+#include "rr_interfaces/srv/joy.hpp"
+#include "rr_interfaces/srv/navigation.hpp"
+#include "rr_interfaces/srv/range.hpp"
 #include "rr_interfaces/srv/state_response.hpp"
 #include "state_validator.hpp"
 
@@ -38,53 +39,53 @@ class RrStateManagerSrv : public rclcpp::Node
    * @brief
    * sets GPS state within the state manager.
    */
-  void set_gps(const std::shared_ptr<rr_interfaces::srv::StateGpsReq::Request> request,
-               std::shared_ptr<rr_interfaces::srv::StateGpsReq::Response> response);
+  void set_gps(const std::shared_ptr<rr_interfaces::srv::Gps::Request> request,
+               std::shared_ptr<rr_interfaces::srv::Gps::Response> response);
 
-  /**
-   * @fn set_gps
-   * @brief
-   * sets Joystick state within the state manager.
-   */
-  void set_joystick(const std::shared_ptr<rr_interfaces::srv::StateJoyReq::Request> request,
-                    std::shared_ptr<rr_interfaces::srv::StateJoyReq::Response> response);
+  // /**
+  //  * @fn set_gps
+  //  * @brief
+  //  * sets Joystick state within the state manager.
+  //  */
+  // void set_joystick(const std::shared_ptr<rr_interfaces::srv::Joy::Request> request,
+  //                   std::shared_ptr<rr_interfaces::srv::Joy::Response> response);
 
-  /**
-   * @fn set_batt_state
-   * @brief
-   * sets battery state
-   */
-  void set_batt_state(const std::shared_ptr<rr_interfaces::srv::StateBattReq::Request> request,
-                      std::shared_ptr<rr_interfaces::srv::StateBattReq::Response> response);
+  // /**
+  //  * @fn set_batt_state
+  //  * @brief
+  //  * sets battery state
+  //  */
+  // void set_batt_state(const std::shared_ptr<rr_interfaces::srv::BatteryState::Request> request,
+  //                     std::shared_ptr<rr_interfaces::srv::BatteryState::Response> response);
 
-  /**
-   * @fn set_image
-   * @brief
-   * sets current image frame in state manager.
-   */
-  void set_image(const std::shared_ptr<rr_interfaces::srv::StateImage::Request> request,
-                 std::shared_ptr<rr_interfaces::srv::StateImage::Response> response);
+  // /**
+  //  * @fn set_image
+  //  * @brief
+  //  * sets current image frame in state manager.
+  //  */
+  // void set_image(const std::shared_ptr<rr_interfaces::srv::Image::Request> request,
+  //                std::shared_ptr<rr_interfaces::srv::Image::Response> response);
 
-  void set_imu(const std::shared_ptr<rr_interfaces::srv::StateImu::Request> request,
-               std::shared_ptr<rr_interfaces::srv::StateImu::Response> response);
+  // void set_imu(const std::shared_ptr<rr_interfaces::srv::Imu::Request> request,
+  //              std::shared_ptr<rr_interfaces::srv::Imu::Response> response);
 
-  void set_range(const std::shared_ptr<rr_interfaces::srv::StateRange::Request> request,
-                 std::shared_ptr<rr_interfaces::srv::StateRange::Response> response);
+  // void set_range(const std::shared_ptr<rr_interfaces::srv::StateRange::Request> request,
+  //                std::shared_ptr<rr_interfaces::srv::StateRange::Response> response);
 
-  /**
-   * @fn get_state
-   * @brief returns current state
-   */
-  void get_state(const std::shared_ptr<rr_interfaces::srv::StateResponse::Request> request,
-                 std::shared_ptr<rr_interfaces::srv::StateResponse::Response> response);
+  // /**
+  //  * @fn get_state
+  //  * @brief returns current state
+  //  */
+  // void get_state(const std::shared_ptr<rr_interfaces::srv::State::Request> request,
+  //                std::shared_ptr<rr_interfaces::srv::State::Response> response);
 
   ~RrStateManagerSrv() = default;
 
   // state variables
-  rclcpp::Service<rr_interfaces::srv::StateGpsReq>::SharedPtr state_gps_req_;
-  rclcpp::Service<rr_interfaces::srv::StateJoyReq>::SharedPtr state_joy_req_;
-  rclcpp::Service<rr_interfaces::srv::StateBattReq>::SharedPtr state_bat_req_;
-  rclcpp::Service<rr_interfaces::srv::StateImage>::SharedPtr state_img_req_;
+  rclcpp::Service<rr_interfaces::srv::Gps>::SharedPtr state_gps_req_;
+  rclcpp::Service<rr_interfaces::srv::Joy>::SharedPtr state_joy_req_;
+  rclcpp::Service<rr_interfaces::srv::BatteryState>::SharedPtr state_bat_req_;
+  rclcpp::Service<rr_interfaces::srv::Image>::SharedPtr state_img_req_;
 
  private:
   long msg_snt_ = 0;
--- src/state_manager.cpp
+++ src/state_manager.cpp
@@ -6,134 +6,138 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 
 // gps setter/getter
-void RrStateManagerSrv::set_gps(
-    const std::shared_ptr<rr_interfaces::srv::StateGpsReq::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateGpsReq::Response> response)
+void RrStateManagerSrv::set_gps(const std::shared_ptr<rr_interfaces::srv::Gps::Request> request,
+                                std::shared_ptr<rr_interfaces::srv::Gps::Response> response)
 {
   std::unique_lock<std::shared_mutex> lock(mutex_);
   buffer_response_.feature_sets.has_gps = true;
-  buffer_response_.gps                  = request->gps;
-  response->buffer_response             = buffer_response_;
+  buffer_response_.gps                  = request->gps_tx;
+  response->gps_rx                      = buffer_response_.gps;
 }
 
-void RrStateManagerSrv::set_joystick(
-    const std::shared_ptr<rr_interfaces::srv::StateJoyReq::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateJoyReq::Response> response)
-{
-  std::unique_lock<std::shared_mutex> lock(mutex_);
-  buffer_response_.feature_sets.has_joy = true;
-  buffer_response_.joystick             = request->joystick;
-  response->buffer_response             = buffer_response_;
-}
+// void RrStateManagerSrv::set_joystick(
+//     const std::shared_ptr<rr_interfaces::srv::StateJoyReq::Request> request,
+//     std::shared_ptr<rr_interfaces::srv::StateJoyReq::Response> response)
+// {
+//   std::unique_lock<std::shared_mutex> lock(mutex_);
+//   buffer_response_.feature_sets.has_joy = true;
+//   buffer_response_.joystick             = request->joystick;
+//   response->buffer_response             = buffer_response_;
+// }
 
-void RrStateManagerSrv::set_batt_state(
-    const std::shared_ptr<rr_interfaces::srv::StateBattReq::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateBattReq::Response> response)
-{
-  std::unique_lock<std::shared_mutex> lock(mutex_);
-  buffer_response_.feature_sets.has_batt_state = true;
-  buffer_response_.batt_state                  = request->batt_state;
-  response->buffer_response                    = buffer_response_;
-}
+// void RrStateManagerSrv::set_batt_state(
+//     const std::shared_ptr<rr_interfaces::srv::StateBattReq::Request> request,
+//     std::shared_ptr<rr_interfaces::srv::StateBattReq::Response> response)
+// {
+//   std::unique_lock<std::shared_mutex> lock(mutex_);
+//   buffer_response_.feature_sets.has_batt_state = true;
+//   buffer_response_.batt_state                  = request->batt_state;
+//   response->buffer_response                    = buffer_response_;
+// }
 
-void RrStateManagerSrv::set_image(
-    const std::shared_ptr<rr_interfaces::srv::StateImage::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateImage::Response> response)
-{
-  std::unique_lock<std::shared_mutex> lock(mutex_);
-  buffer_response_.feature_sets.has_img = true;
-  buffer_response_.img                  = request->img;
-  response->buffer_response             = buffer_response_;
-}
+// void RrStateManagerSrv::set_image(
+//     const std::shared_ptr<rr_interfaces::srv::StateImage::Request> request,
+//     std::shared_ptr<rr_interfaces::srv::StateImage::Response> response)
+// {
+//   std::unique_lock<std::shared_mutex> lock(mutex_);
+//   buffer_response_.feature_sets.has_img = true;
+//   buffer_response_.img                  = request->img;
+//   response->buffer_response             = buffer_response_;
+// }
 
-void RrStateManagerSrv::set_imu(
-    const std::shared_ptr<rr_interfaces::srv::StateImu::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateImu::Response> response)
-{
-  std::unique_lock<std::shared_mutex> lock(mutex_);
-  buffer_response_.feature_sets.has_imu = true;
-  buffer_response_.imu                  = request->imu;
-  response->buffer_response             = buffer_response_;
-}
+// void RrStateManagerSrv::set_imu(
+//     const std::shared_ptr<rr_interfaces::srv::StateImu::Request> request,
+//     std::shared_ptr<rr_interfaces::srv::StateImu::Response> response)
+// {
+//   std::unique_lock<std::shared_mutex> lock(mutex_);
+//   buffer_response_.feature_sets.has_imu = true;
+//   buffer_response_.imu                  = request->imu;
+//   response->buffer_response             = buffer_response_;
+// }
 
-void RrStateManagerSrv::set_range(
-    const std::shared_ptr<rr_interfaces::srv::StateRange::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateRange::Response> response)
-{
-  std::unique_lock<std::shared_mutex> lock(mutex_);
-  sensor_msgs::msg::Range range = request->range;
-  if (std::find(RANGES_LINKS_.begin(), RANGES_LINKS_.end(), range.header.frame_id) ==
-      RANGES_LINKS_.end())
-  {
-    RCLCPP_ERROR(logger_, "unsupported range sensor, %s not found in supported",
-                 range.header.frame_id.c_str());
-  }
+// void RrStateManagerSrv::set_range(
+//     const std::shared_ptr<rr_interfaces::srv::StateRange::Request> request,
+//     std::shared_ptr<rr_interfaces::srv::StateRange::Response> response)
+// {
+//   std::unique_lock<std::shared_mutex> lock(mutex_);
+//   sensor_msgs::msg::Range range = request->range;
+//   if (std::find(RANGES_LINKS_.begin(), RANGES_LINKS_.end(), range.header.frame_id) ==
+//       RANGES_LINKS_.end())
+//   {
+//     RCLCPP_ERROR(logger_, "unsupported range sensor, %s not found in supported",
+//                  range.header.frame_id.c_str());
+//   }
 
-  buffer_response_.feature_sets.has_ranges = true;
-  for (size_t i = 0; i < buffer_response_.ranges.size(); ++i)
-  {
-    if (buffer_response_.ranges.at(i).header.frame_id == range.header.frame_id)
-    {
-      buffer_response_.ranges[i] = range;
-      response->buffer_response  = buffer_response_;
-      return;
-    }
-  }
+//   buffer_response_.feature_sets.has_ranges = true;
+//   for (size_t i = 0; i < buffer_response_.ranges.size(); ++i)
+//   {
+//     if (buffer_response_.ranges.at(i).header.frame_id == range.header.frame_id)
+//     {
+//       buffer_response_.ranges[i] = range;
+//       response->buffer_response  = buffer_response_;
+//       return;
+//     }
+//   }
 
-  buffer_response_.ranges.resize(buffer_response_.ranges.size() + 1);
-  buffer_response_.ranges[buffer_response_.ranges.size() - 1] = range;
-  response->buffer_response                                   = buffer_response_;
-}
+//   buffer_response_.ranges.resize(buffer_response_.ranges.size() + 1);
+//   buffer_response_.ranges[buffer_response_.ranges.size() - 1] = range;
+//   response->buffer_response                                   = buffer_response_;
+// }
 
-/*
- * Set UUID and a few other variables to identify the buffer. To track the request there is some 
- * details that be recorded such as trace_guid, and span_guid.
- * 
- * The span GUID can be set here.
- */
-void RrStateManagerSrv::get_state(
-    const std::shared_ptr<rr_interfaces::srv::StateResponse::Request> request,
-    std::shared_ptr<rr_interfaces::srv::StateResponse::Response> response)
-{
-  if (!validator_.validate_uuid(request->buffer_request.request_id, false)) 
-  {
-    RCLCPP_WARN(this->logger_, "request recieved that did not have a request id");
-  }
+// /*
+//  * Set UUID and a few other variables to identify the buffer. To track the request there is some
+//  * details that be recorded such as trace_guid, and span_guid.
+//  *
+//  * The span GUID can be set here.
+//  */
+// void RrStateManagerSrv::get_state(
+//     const std::shared_ptr<rr_interfaces::srv::StateResponse::Request> request,
+//     std::shared_ptr<rr_interfaces::srv::StateResponse::Response> response)
+// {
+//   if (!validator_.validate_uuid(request->buffer_request.request_id, false))
+//   {
+//     RCLCPP_WARN(this->logger_, "request recieved that did not have a request id");
+//   }
 
-  std::shared_lock<std::shared_mutex> lock(mutex_);
-  boost::uuids::uuid boost_uuid = boost::uuids::random_generator()();
-  unique_identifier_msgs::msg::UUID uuid_msg;
-  std::copy(boost_uuid.begin(), boost_uuid.end(), uuid_msg.uuid.begin());
-  buffer_response_.guid = uuid_msg;
-  buffer_response_.header.stamp = this->now();
-  buffer_response_.header.frame_id = rr_constants::LINK_STATE;
-  msg_snt_++;
-  RCLCPP_DEBUG(logger_, "sending current state sequence: %ld", msg_snt_);
-  buffer_response_.seq = msg_snt_;
-  response->buffer_response = buffer_response_;
-}
+//   std::shared_lock<std::shared_mutex> lock(mutex_);
+//   boost::uuids::uuid boost_uuid = boost::uuids::random_generator()();
+//   unique_identifier_msgs::msg::UUID uuid_msg;
+//   std::copy(boost_uuid.begin(), boost_uuid.end(), uuid_msg.uuid.begin());
+//   buffer_response_.guid = uuid_msg;
+//   buffer_response_.header.stamp = this->now();
+//   buffer_response_.header.frame_id = rr_constants::LINK_STATE;
+//   msg_snt_++;
+//   RCLCPP_DEBUG(logger_, "sending current state sequence: %ld", msg_snt_);
+//   buffer_response_.seq = msg_snt_;
+//   response->buffer_response = buffer_response_;
+// }
 
 void RrStateManagerSrv::init_services()
 {
-  RCLCPP_INFO(logger_, "creating services");
-  RCLCPP_INFO(logger_, "creating state_manager::%s", rr_constants_state_mgr::STATE_GPS_REQ.c_str());
-  state_gps_req_ = this->create_service<rr_interfaces::srv::StateGpsReq>(
-      rr_constants_state_mgr::STATE_GPS_REQ, std::bind(&RrStateManagerSrv::set_gps, this, _1, _2));
+  //   RCLCPP_INFO(logger_, "creating services");
+  //   RCLCPP_INFO(logger_, "creating state_manager::%s",
+  //   rr_constants_state_mgr::STATE_GPS_REQ.c_str()); state_gps_req_ =
+  //   this->create_service<rr_interfaces::srv::StateGpsReq>(
+  //       rr_constants_state_mgr::STATE_GPS_REQ, std::bind(&RrStateManagerSrv::set_gps, this, _1,
+  //       _2));
 
-  RCLCPP_INFO(logger_, "creating state_manager::%s", rr_constants_state_mgr::STATE_JOY_REQ.c_str());
-  state_joy_req_ = this->create_service<rr_interfaces::srv::StateJoyReq>(
-      rr_constants_state_mgr::STATE_JOY_REQ,
-      std::bind(&RrStateManagerSrv::set_joystick, this, _1, _2));
+  //   RCLCPP_INFO(logger_, "creating state_manager::%s",
+  //   rr_constants_state_mgr::STATE_JOY_REQ.c_str()); state_joy_req_ =
+  //   this->create_service<rr_interfaces::srv::StateJoyReq>(
+  //       rr_constants_state_mgr::STATE_JOY_REQ,
+  //       std::bind(&RrStateManagerSrv::set_joystick, this, _1, _2));
 
-  RCLCPP_INFO(logger_, "creating state_manager::%s", rr_constants_state_mgr::STATE_BAT_REQ.c_str());
-  state_bat_req_ = this->create_service<rr_interfaces::srv::StateBattReq>(
-      rr_constants_state_mgr::STATE_BAT_REQ,
-      std::bind(&RrStateManagerSrv::set_batt_state, this, _1, _2));
+  //   RCLCPP_INFO(logger_, "creating state_manager::%s",
+  //   rr_constants_state_mgr::STATE_BAT_REQ.c_str()); state_bat_req_ =
+  //   this->create_service<rr_interfaces::srv::StateBattReq>(
+  //       rr_constants_state_mgr::STATE_BAT_REQ,
+  //       std::bind(&RrStateManagerSrv::set_batt_state, this, _1, _2));
 
-  RCLCPP_INFO(logger_, "creating state_manager::%s", rr_constants_state_mgr::STATE_IMG_REQ.c_str());
-  state_img_req_ = this->create_service<rr_interfaces::srv::StateImage>(
-      rr_constants_state_mgr::STATE_IMG_REQ,
-      std::bind(&RrStateManagerSrv::set_image, this, _1, _2));
+  //   RCLCPP_INFO(logger_, "creating state_manager::%s",
+  //   rr_constants_state_mgr::STATE_IMG_REQ.c_str()); state_img_req_ =
+  //   this->create_service<rr_interfaces::srv::StateImage>(
+  //       rr_constants_state_mgr::STATE_IMG_REQ,
+  //       std::bind(&RrStateManagerSrv::set_image, this, _1, _2));
 }
 
 /*
