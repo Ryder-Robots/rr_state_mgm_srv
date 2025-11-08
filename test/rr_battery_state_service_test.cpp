409dc3239c449fc1d76ff9aacd33a24e75fbe487
Aaron Spiteri <azzmosphere@gmail.com>
2025-11-08 22:15:37 +1100

    added battery status unit test

--- test/rr_battery_state_service_test.cpp
+++ test/rr_battery_state_service_test.cpp
@@ -62,6 +62,7 @@ TEST_F(TestController, battery_state_set)
   battery_state.cell_temperature[2] = 20;
   battery_state.location            = rr_constants::BATT_LOC_BASE;
   battery_state.serial_number       = "FOOSERIAL1111";
+  battery_state.present = true;
 
   rr_interfaces::srv::BatteryState::Request request;
   rr_interfaces::srv::BatteryState::Response response;
@@ -77,7 +78,19 @@ TEST_F(TestController, battery_state_set)
   object_under_test_->set_batt_state(req_sh, res_sh);
   sensor_msgs::msg::BatteryState res = res_sh->batt_state_rx;
 
-  //   EXPECT_EQ(res_sh->batt_state_rx, rr_constants::LINK_BATT_STATE);
+  EXPECT_EQ(res.header.frame_id, rr_constants::LINK_BATT_STATE);
+  EXPECT_EQ(res.header.stamp, current_time);
+  EXPECT_EQ(res.voltage, battery_state.voltage);
+  EXPECT_EQ(res.temperature, battery_state.temperature);
+  EXPECT_EQ(res.current, battery_state.current);
+  EXPECT_EQ(res.charge, battery_state.charge);
+  EXPECT_EQ(res.capacity, battery_state.capacity);
+  EXPECT_EQ(res.design_capacity, battery_state.design_capacity);
+  EXPECT_EQ(res.percentage, battery_state.percentage);
+  EXPECT_EQ(res.power_supply_status, battery_state.power_supply_status);
+  EXPECT_EQ(res.power_supply_health, battery_state.power_supply_health);
+  EXPECT_EQ(res.power_supply_technology, battery_state.power_supply_technology);
+  EXPECT_TRUE(res.present);
 
   // TEST IT
   GTEST_EXPECT_TRUE(true);
