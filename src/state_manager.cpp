d8591541359ce81ca6027d191fb6409682aaeb29
Aaron Spiteri <azzmosphere@gmail.com>
2025-11-20 23:21:56 +1100

    added copywrite and remofed some uneeded code

--- src/state_manager.cpp
+++ src/state_manager.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2025 Ryder Robots
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "rr_state_mgm_srv/state_manager.hpp"
 
 using namespace rr_state_manager::rr_state_manager_node;
@@ -67,6 +87,8 @@ rclcpp::QoS RrStateManagerSrv::configure_qos()
 void RrStateManagerSrv::get_state_frame(const std::shared_ptr<rr_interfaces::srv::State::Request> req,
                const std::shared_ptr<rr_interfaces::srv::State::Response> res)
 {
+  std::shared_lock<std::shared_mutex> lock(*mutex_);
+
   boost::uuids::random_generator generator;
   boost::uuids::uuid b_uuid = generator();
   unique_identifier_msgs::msg::UUID guid;
